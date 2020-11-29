2dc4d9965efd25e10402c779f66c38202086e8c5
Gerson <lindorjr.0317@gmail.com>
2020-11-29 15:59:45 -0800

    adding config.js file from local environment

---
+++ functions/config.js
@@ -0,0 +1,11 @@
+/*
+ Contains your firebase API & SERVICE Credentials
+ 
+*/
+const secretKeyPath = require('./secretKey');
+
+module.exports = {
+    CLIENT_ID: "610981118515-qjg25168rofh9s2bkf6ec53lk46sp4vn.apps.googleusercontent.com",
+    CLIENT_SECRET: "VKbl-GkW7MuDS-uPdvqZe3DH",
+    REDIRECT_URL: "http://localhost:5000/oauth2callback"
+}
