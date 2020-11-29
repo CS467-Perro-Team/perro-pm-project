2184608700065a8ab2dd4867c1f1dbdff0f03ead
nwarnemun <32422911+nwarnemun@users.noreply.github.com>
2020-11-28 21:38:33 -0800

    Google Auth, Session & User update
    Added Config file - must be updated with personal client id and client secret information.
    
    Updated .hbs files for sign out functionality & data references.

---
+++ functions/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  	client_id: "<clientid>.apps.googleusercontent.com",
+  	project_id: "", // The name of your project
+    	auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    	token_uri: "https://oauth2.googleapis.com/token",
+    	auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    	client_secret: "",
+    	session_secret: ""
+};
