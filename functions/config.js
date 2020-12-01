c6ef97aa465666ee6f14a190c583bc5ed3e38984
Gerson <lindorjr.0317@gmail.com>
2020-12-01 02:04:52 -0800

    removed personal credentials

--- functions/config.js
+++ functions/config.js
@@ -1,4 +1,3 @@
-/*
 module.exports = {
   	client_id: "<clientid>.apps.googleusercontent.com",
   	project_id: "", // The name of your project
@@ -7,15 +6,4 @@ module.exports = {
     	auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
     	client_secret: "",
     	session_secret: ""
-};
-*/
-
-module.exports = {
-	client_id: "610981118515-qjg25168rofh9s2bkf6ec53lk46sp4vn.apps.googleusercontent.com",
-	project_id: "perro-pm-project", // The name of your project
-	  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-	  token_uri: "https://oauth2.googleapis.com/token",
-	  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-	  client_secret: "VKbl-GkW7MuDS-uPdvqZe3DH",
-	  session_secret: "betterMakeIt"
 };
